f9fb4fe506c87161552315decb3593a8c419534f
zabbix agent <zabbixa@srv-yum-001.intra.rotra.nl>
2018-07-11 13:57:15 +0200

    added oracle object status counts per database per schema, exceptions pytds

--- bin/drivererrors/pytds.py
+++ bin/drivererrors/pytds.py
@@ -1,6 +1,14 @@
 def db_errorcode(driver, excep):
     """pass exception code and message from various drivers in standard way"""
-    return excep.msg_no, excep.args[0]
+    try:
+        return excep.msg_no, excep.args[0]
+    except AttributeError:
+        if type(excep) is LoginError:
+            return 1031, excep.args[0]
+        else:
+            print("type {type}".format(type(excep)))
+            print(excep)
+            print(dir(excep))
 
 def db_error_needs_new_session(driver, code):
     """some errors justify a new database connection. In that case return true"""
