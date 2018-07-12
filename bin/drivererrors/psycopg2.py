fd61b53a6acd1991460e58fdb8940cfd9cafe693
Ronald Rood <ronr@ronr.nl>
2018-07-12 21:09:26 +0200

    check None type in exception code

--- bin/drivererrors/psycopg2.py
+++ bin/drivererrors/psycopg2.py
@@ -1,6 +1,10 @@
 def db_errorcode(driver, excep):
     """pass exception code and message from various drivers in standard way"""
-    return excep.pgcode, excep.args[0]
+    if excep.pgcode is None:
+        c = 1031
+    else:
+        c = excep.pgcode
+    return c, str(excep.args[0])
 
 def db_error_needs_new_session(driver, code):
     """some errors justify a new database connection. In that case return true"""
