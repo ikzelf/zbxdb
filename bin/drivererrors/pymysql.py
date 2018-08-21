a9bdca33661d3170421652a3baa584fbee0c75c1
Ronald Rood <ronr@madrid.spain>
2018-08-21 13:15:36 +0200

    new connection after sqltimeout

--- bin/drivererrors/pymysql.py
+++ bin/drivererrors/pymysql.py
@@ -10,6 +10,8 @@ def db_errorcode(driver, excep):
 
 def db_error_needs_new_session(driver, code):
     """some errors justify a new database connection. In that case return true"""
+    if str(code) in ("2013"):
+        return True
     if driver == "Cx_Oracle":
         if code in(28, 1012, 1041, 3113, 3114, 3135):
             return True
