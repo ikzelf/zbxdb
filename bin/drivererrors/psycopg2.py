7bf2e43e175ec8e3249a97f851ebbfce5b448d8b
Ronald Rood <ronr@madrid.spain>
2018-08-21 13:17:18 +0200

    new connection after dbserver restart

--- bin/drivererrors/psycopg2.py
+++ bin/drivererrors/psycopg2.py
@@ -1,5 +1,6 @@
 def db_errorcode(driver, excep):
     """pass exception code and message from various drivers in standard way"""
+    # print ("db_errorcode ***{}***{}***\n".format(excep, excep.pgcode))
     if excep.pgcode is None:
         c = 1031
     else:
@@ -8,4 +9,6 @@ def db_errorcode(driver, excep):
 
 def db_error_needs_new_session(driver, code):
     """some errors justify a new database connection. In that case return true"""
+    if code in ("57P01"):
+        return True
     return False
