4f0082d5d95162e1fb11f3d8cc0aad6a00e36bfd
Ronald Rood <ronr@ronr.nl>
2018-09-17 15:38:16 +0200

    continue trying to connect after hba.conf errors

--- bin/drivererrors/psycopg2.py
+++ bin/drivererrors/psycopg2.py
@@ -2,10 +2,10 @@ def db_errorcode(driver, excep):
     """pass exception code and message from various drivers in standard way"""
     # print ("db_errorcode ***{}***{}***\n".format(excep, excep.pgcode))
     if excep.pgcode is None:
-        c = 1031
+        c = "zbxdb-1031"
     else:
         c = excep.pgcode
-    return c, str(excep.args[0])
+    return str(c), str(excep.args[0])
 
 def db_error_needs_new_session(driver, code):
     """some errors justify a new database connection. In that case return true"""
