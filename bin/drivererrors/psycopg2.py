5e22bcecd585adefb95ba5bfb7af099a56336b83
Ronald Rood <ronr@ronr.nl>
2018-09-21 10:06:48 +0200

    improved response on timeout and network errors (firewall as test)

--- bin/drivererrors/psycopg2.py
+++ bin/drivererrors/psycopg2.py
@@ -2,12 +2,12 @@ def db_errorcode(driver, excep):
     """pass exception code and message from various drivers in standard way
        driver is the loaded driver object"""
     # print ("driver: {}".format(driver))
-    # print ("db_errorcode ***{}***{}***\n".format(excep, excep.pgcode))
+    print ("db_errorcode ***{}***{}***\n".format(excep, excep.pgcode))
     # how to catch one of:
     # - psycopg2.OperationalError: FATAL:  no pg_hba.conf entry for host
     # - psycopg2.OperationalError: server closed the connection unexpectedly
     # - psycopg2.OperationalError: could not connect to server: Connection refused
-    if driver.OperationalError:
+    if driver.OperationalError and (not excep.pgcode):
         c = 1001
     elif excep.pgcode is None:
         c = 1031
