913d9251bcdf040e95db3ea5b87039174df14d90
Ronald Rood <ronr@ronr.nl>
2018-11-09 14:06:50 +0100

    hopefully an improvement for error handling

--- bin/drivererrors/pytds.py
+++ bin/drivererrors/pytds.py
@@ -1,14 +1,17 @@
 def db_errorcode(driver, excep):
     """pass exception code and message from various drivers in standard way"""
-    try:
+    # print('db_errorcode:{}\n'.format(excep))
+    # print(type(excep))
+    # print(dir(excep))
+    if "LoginError" in str(type(excep)):
+        return 1031, excep.args[0]
+    elif "OperationalError"  in str(type(excep)):
         return excep.msg_no, excep.args[0]
-    except AttributeError:
-        if type(excep) is LoginError:
-            return 1031, excep.args[0]
-        else:
-            print("type {type}".format(type(excep)))
-            print(excep)
-            print(dir(excep))
+    else:
+        print("type {}".format(type(excep)))
+        print(excep)
+        print(dir(excep))
+        return excep.msg_no, excep.args[0]
 
 def db_error_needs_new_session(driver, code):
     """some errors justify a new database connection. In that case return true"""
