e478764358d621ae4547317f8df547d86b0c7984
Ronald Rood <ronr@ronr.nl>
2019-01-16 17:09:49 +0100

    messed up sorry

--- bin/drivererrors/pytds.py
+++ bin/drivererrors/pytds.py
@@ -1,12 +1,10 @@
 def db_errorcode(driver, excep):
     """pass exception code and message from various drivers in standard way"""
-<<<<<<< HEAD
     print('db_errorcode:{}\n'.format(excep))
     if excep.LoginError:
         return 1031, excep.args[0]
     else:
         print("type {type}".format(type(excep)))
-=======
     # print('db_errorcode:{}\n'.format(excep))
     # print(type(excep))
     # print(dir(excep))
@@ -16,7 +14,6 @@ def db_errorcode(driver, excep):
         return excep.msg_no, excep.args[0]
     else:
         print("type {}".format(type(excep)))
->>>>>>> 7bce01271689d0ff13d8f0ab99e70cea4e23ed73
         print(excep)
         print(dir(excep))
         return excep.msg_no, excep.args[0]
