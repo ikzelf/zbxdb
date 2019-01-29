294186373cd882ffb9724b20cc2d832fa3b99da4
Ronald Rood <ik_zelf@me.com>
2019-01-29 10:42:46 +0100

    error handling and quoting of sql output

--- bin/drivererrors/pytds.py
+++ bin/drivererrors/pytds.py
@@ -1,20 +1,12 @@
 def db_errorcode(driver, excep):
     """pass exception code and message from various drivers in standard way"""
-    print('db_errorcode:{}\n'.format(excep))
-    if excep.LoginError:
-        return 1031, excep.args[0]
-    else:
-        print("type {type}".format(type(excep)))
-    # print('db_errorcode:{}\n'.format(excep))
-    # print(type(excep))
-    # print(dir(excep))
     if "LoginError" in str(type(excep)):
         return 1031, excep.args[0]
     elif "OperationalError"  in str(type(excep)):
         return excep.msg_no, excep.args[0]
     else:
-        print("type {}".format(type(excep)))
-        print(excep)
+        print('db_errorcode:{}\n'.format(excep))
+        print(type(excep))
         print(dir(excep))
         return excep.msg_no, excep.args[0]
 
