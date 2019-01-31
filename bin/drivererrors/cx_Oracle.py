b773314b28aa00c609cd6a2742f444e9b50b3e34
Ronald Rood <ik_zelf@me.com>
2019-01-31 15:18:30 +0100

    added ora-1000 as reason for new connection

--- bin/drivererrors/cx_Oracle.py
+++ bin/drivererrors/cx_Oracle.py
@@ -5,8 +5,11 @@ def db_errorcode(driver, excep):
     return ERROR.code, str(excep.args[0])
 
 def db_error_needs_new_session(driver, code):
-    """some errors justify a new database connection. In that case return true"""
-    if code in(28, 1012, 1041, 3113, 3114, 3117, 3135, 12153):
+    """some errors justify a new database connection. In that case return true
+        mostly due to strange issues
+        added 1000 (open_cursors exceeded) after adding a test over dg4odbc
+    """
+    if code in(28, 1000, 1012, 1041, 3113, 3114, 3117, 3135, 12153):
         print('db_error_needs_new_session:{}\n'.format(code))
         return True
     if code == 15000:
