42c80e4918d577f1a5bc9e9f7d734c7d633f16ab
Ronald Rood <ik_zelf@me.com>
2019-02-22 12:20:13 +0100

    Improved timeout handling

--- bin/drivererrors/pytds.py
+++ bin/drivererrors/pytds.py
@@ -1,15 +1,24 @@
+import sys
+
 def db_errorcode(driver, excep):
     """pass exception code and message from various drivers in standard way"""
     if "LoginError" in str(type(excep)):
-        return 1031, excep.args[0]
+        return "LoginError", excep.args[0]
     elif "OperationalError"  in str(type(excep)):
         return excep.msg_no, excep.args[0]
+    elif "ConnectionResetError" in str(type(excep)):
+        return "ConnectionResetError", str(excep.args[0])
+    elif "timed out" == str(excep.args[0]):
+        return type(excep), excep.args[0]
     else:
         print('db_errorcode:{}\n'.format(excep))
         print(type(excep))
         print(dir(excep))
-        return excep.msg_no, excep.args[0]
+        sys.stdout.flush()
+        return excep.msg_no, str(excep.args[0])
 
 def db_error_needs_new_session(driver, code):
     """some errors justify a new database connection. In that case return true"""
+    if code == "ConnectionResetError":
+        return True
     return False
