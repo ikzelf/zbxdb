94b0a082321e6245e2933d0ee50b513ef77ec078
Ronald Rood <ronr@ronr.nl>
2019-05-02 15:24:09 +0200

    pepped up and added appname in connection

--- bin/dbconnections/mssql.py
+++ bin/dbconnections/mssql.py
@@ -1,43 +1,49 @@
+""" functions to return zbxdb info needed for mssql"""
+
+
 def connection_info(conn):
     """get connection info from connected database"""
     conn_info = {'dbversion': "", 'sid': 0, 'instance_type': "rdbms",
                  'serial': 0, 'db_role': "primary", 'uname': "",
                  'iname': ""}
-    C = conn.cursor()
-    # C.execute("select substring(version from '[0-9]+') from version()")
-    # C.execute("""SELECT SUBSTRING(ver, 1, CHARINDEX('.', ver) - 1) FROM (SELECT CAST(serverproperty('ProductVersion') AS nvarchar) ver)""")
+    _c = conn.cursor()
 
-    C.execute("""
+    _c.execute("""
 SELECT LEFT(cast(serverproperty('ProductVersion') as varchar), CHARINDEX('.',
 cast(serverproperty('ProductVersion') as varchar)) - 1)""")
 
-    DATA = C.fetchone()
+    data = _c.fetchone()
 
-    conn_info['dbversion'] = DATA[0]
+    conn_info['dbversion'] = data[0]
 
     # C.execute("select pg_backend_pid()")
     # DATA = C.fetchone()
     # conn_info['sid'] = DATA[0]
-    C.execute("SELECT @@servername, ORIGINAL_LOGIN()")
-    DATA = C.fetchone()
-    conn_info['iname'] = DATA[0]
-    conn_info['uname'] = DATA[1]
-    C.close()
+    _c.execute("SELECT @@servername, ORIGINAL_LOGIN()")
+    data = _c.fetchone()
+    conn_info['iname'] = data[0]
+    conn_info['uname'] = data[1]
+    _c.close()
+
     return conn_info
 
-# def connect_string(c):
-#     return 'server="'+ c['server']+'",'+\
-#             'database="'+ c['db_name']+'",'+\
-#             'port="'+ c['server_port']+'",'+\
-#             'user="'+ c['username']+'",'+ 'password="'+ c['password']+'"'
-def connect_string(c):
-    return "{usr}/{pwd}@{url},{port}/{db}".format(usr=c['username'],pwd=c['password'],
-                      url=c['server'],port=c['server_port'],db=c['db_name'])
-def connect(db, c):
-    return db.connect(server=c['server'],
-                      database=c['db_name'],
-                      port=c['server_port'],
-                      user=c['username'],
-                      password=c['password'],
-                      timeout=c['sqltimeout']
-                    )
+
+def connect_string(_c):
+    """construct connect string, just incase"""
+
+    return "{usr}/{pwd}@{url},{port}/{db}".format(usr=_c['username'], pwd=_c['password'],
+                                                  url=_c['server'], port=_c['server_port'],
+                                                  db=_c['db_name'])
+
+
+def connect(_db, _c):
+    """the actual connect, also specifying the appname"""
+
+    return _db.connect(server=_c['server'],
+                       database=_c['db_name'],
+                       port=_c['server_port'],
+                       user=_c['username'],
+                       password=_c['password'],
+                       timeout=_c['sqltimeout'],
+                       appname=_c['ME']
+                       )
