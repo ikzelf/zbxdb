494f118045913c8f529235341d3d01609017d694
Ronald Rood <ronr@ronr.nl>
2019-05-03 09:49:19 +0200

    pepped code and  added program_name in connection

--- bin/dbconnections/mysql.py
+++ bin/dbconnections/mysql.py
@@ -1,48 +1,58 @@
+""" mysql specific connection methods"""
+import pymysql
+
+
 def connection_info(conn):
     """get connection info from connected database"""
     conn_info = {'dbversion': "", 'sid': 0, 'instance_type': "rdbms",
                  'serial': 0, 'db_role': "primary", 'uname': "",
                  'iname': ""}
-    C = conn.cursor()
+    _c = conn.cursor()
 
-    C.execute("select left(version(),locate('.',version())-1)")
+    _c.execute("select left(version(),locate('.',version())-1)")
 
-    DATA = C.fetchone()
+    _data = _c.fetchone()
 
-    conn_info['dbversion'] = DATA[0]
+    conn_info['dbversion'] = _data[0]
 
-    C.execute("select concat(@@basedir,':',@@port), current_user()")
-    DATA = C.fetchone()
-    conn_info['iname'] = DATA[0]
-    conn_info['uname'] = DATA[1]
+    _c.execute("select concat(@@basedir,':',@@port), current_user()")
+    _data = _c.fetchone()
+    conn_info['iname'] = _data[0]
+    conn_info['uname'] = _data[1]
 
     try:
-      C.execute("select count(*) from performance_schema.replication_applier_status")
-      DATA = C.fetchone()
-      if DATA[0] > 0:
-          conn_info['db_role'] = "slave"
-    except:
-      # a bit dirty ... assume primary replication_applier_status is pretty new
-      pass
+        _c.execute(
+            "select count(*) from performance_schema.replication_applier_status")
+        _data = _c.fetchone()
 
-    C.close()
+        if _data[0] > 0:
+            conn_info['db_role'] = "slave"
+    except pymysql.ProgrammingError:
+        # a bit dirty ... assume primary replication_applier_status is pretty new
+        pass
+
+    _c.close()
+
     return conn_info
 
-# def connect_string(c):
-#     return 'server="'+ c['server']+'",'+\
-#             'database="'+ c['db_name']+'",'+\
-#             'port="'+ c['server_port']+'",'+\
-#             'user="'+ c['username']+'",'+ 'password="'+ c['password']+'"'
-def connect_string(c):
-    return "{usr}/{pwd}@{url},{port}/{db}".format(usr=c['username'],pwd=c['password'],
-                      url=c['server'],port=c['server_port'],db=c['db_name'])
-def connect(db, c):
-    dbc = db.connect(host=c['server'],
-                      user=c['username'],
-                      password=c['password'],
-                      db=c['db_name'],
-                      port=int(c['server_port']),
-                      read_timeout=int(c['sqltimeout'])#,
-                    #  cursorclass=pymysql.cursors.DictCursor
-                    )
+
+def connect_string(_c):
+    """ return connect string"""
+
+    return "{usr}/{pwd}@{url},{port}/{db}".format(usr=_c['username'], pwd=_c['password'],
+                                                  url=_c['server'], port=_c['server_port'],
+                                                  db=_c['db_name'])
+
+
+def connect(_db, _c):
+    """  the actual connect"""
+    dbc = _db.connect(host=_c['server'],
+                      user=_c['username'],
+                      password=_c['password'],
+                      db=_c['db_name'],
+                      port=int(_c['server_port']),
+                      read_timeout=int(_c['sqltimeout']),
+                      program_name=_c['ME']
+                      )
+
     return dbc
