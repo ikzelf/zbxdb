db58a7b45297d4bd8b1e82266f03e084d06003df
Ronald Rood <ronr@ronr.nl>
2019-10-18 20:22:39 +0200

    early start of db2

---
+++ bin/dbconnections/db2.py
@@ -0,0 +1,60 @@
+""" oracle specific connection methods"""
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+def current_role(conn, info):
+    """return current role of database"""
+
+    _c_role = "primary"
+
+    return _c_role
+
+
+def connection_info(con):
+    """get connection info from connected database"""
+    conn_info = {'dbversion': "", 'sid': 0, 'instance_type': "rdbms",
+                 'serial': 0, 'db_role': "", 'uname': "",
+                 'iname': ""}
+    _c = con.cursor()
+    _c.execute("""SELECT 
+    substr(replace(service_level,'DB2 v',''),
+            1,
+            locate_in_string(replace(service_level,'DB2 v',''),'.')-1
+          )
+     , inst_name
+     , SESSION_USER
+     FROM SYSIBMADM.ENV_INST_INFO
+     """)
+
+    _data = _c.fetchone()
+
+    conn_info['dbversion'] = _data[0]
+    conn_info['instance_type'] = "rdbms"
+    conn_info['iname'] = _data[1]
+    conn_info['uname'] = _data[2]
+    conn_info['db_role'] =  "primary"
+
+    _c.close()
+
+    return conn_info
+
+
+def connect_string(config):
+    """return connect string"""
+
+    return "DATABASE={};HOSTNAME={};PORT={};PROTOCOL=TCPIP;UID={};PWD={};".format(
+            config['db_name'], config['server'], config['server_port'],
+            config['username'], config['password'])
+
+
+def connect(_db, _c):
+    """the actual connect"""
+    import ibm_db_dbi
+
+    LOGGER.debug("Connecting %s as %s",
+                 connect_string(_c).replace(_c['password'], '*X*X*X*'),'user')
+
+    con = _db.pconnect(connect_string(_c),"","")
+    return ibm_db_dbi.Connection(con)
---
+++ bin/drivererrors/ibm_db.py
@@ -0,0 +1,39 @@
+"""standardize driver errors for zbxdb"""
+import logging
+
+LOGGER = logging.getLogger(__name__)
+def fullname(_o):
+    """return fqn of  this module"""
+    # _o.__module__ + "." + _o.__class__.__qualname__ is an example in
+    # this context of H.L. Mencken's "neat, plausible, and wrong."
+    # Python makes no guarantees as to whether the __module__ special
+    # attribute is defined, so we take a more circumspect approach.
+    # Alas, the module name is explicitly excluded from __qualname__
+    # in Python 3.
+    module = _o.__class__.__module__
+    if module is None or module == str.__class__.__module__:
+        return _o.__class__.__name__  # Avoid reporting __builtin__
+    return module + '.' + _o.__class__.__name__
+
+def db_errorcode(driver, excep):
+    """pass exception code and message from various drivers in standard way"""
+    LOGGER.debug('db_errorcode: %s\n', excep)
+    if fullname(excep) == "ConnectionResetError":
+        return fullname(excep), str(excep.args[0])
+    _error, = excep.args
+    return _error.code, str(excep.args[0])
+
+def db_error_needs_new_session(driver, code):
+    """some errors justify a new database connection. In that case return true
+        mostly due to strange issues
+        added 1000 (open_cursors exceeded) after adding a test over dg4odbc
+        added 55524 (Too many recursive autonomous transactions for temporary
+        undo) after adding a test over dg4odbc
+    """
+    if code in(28, 1000, 1012, 1041, 3113, 3114, 3117, 3135, 12153, 55524):
+        # print('db_error_needs_new_session:{}\n'.format(code))
+        return True
+    if code == 15000:
+        LOGGER.critical('asm requires sysdba role\n')
+        return True
+    return False
